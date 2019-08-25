31fa9f549825e3d07c3b9780bcaad286264f9006
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2019-08-25 16:54:59 +0300

    Build helper

---
+++ Assets/Scripts/Editor/BuildHelper.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+class BuildHelper
+{
+     static void PerformBuild ()
+     {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "iOSBuild";
+        buildPlayerOptions.target = BuildTarget.iOS;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+     }
+}
