37adfcb2805328c0172ca4d1e13547f0035ffe73
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2019-08-28 23:22:28 +0300

    Car engine

---
+++ Assets/Scripts/CarEngine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour
+{
+    public WheelCollider wheelL;
+    public WheelCollider wheelR;
+    public void Accelerate()
+    {
+        wheelL.motorTorque = 100;
+        wheelR.motorTorque = 100;
+    }
+
+    public void SteerRight()
+    {
+        wheelL.steerAngle = 30;
+        wheelR.steerAngle = 30;
+    }
+
+    public void SteerLeft()
+    {
+        wheelL.steerAngle = -30;
+        wheelR.steerAngle = -30;
+    }
+}
---
+++ Assets/Scripts/CarWheel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheel : MonoBehaviour
+{
+    WheelCollider targetWheel;
+
+    Vector3 wheelPosiiton = new Vector3();
+    Quaternion wheelRotation = new Quaternion();
+
+    private void Update()
+    {
+        targetWheel.GetWorldPose(out wheelPosiiton, out wheelRotation);
+        transform.position = wheelPosiiton;
+        transform.rotation = wheelRotation;
+    }
+}
+
